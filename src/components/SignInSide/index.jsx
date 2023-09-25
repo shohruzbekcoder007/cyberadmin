936caf19fd8227557f05c283794b4739bbd68af1
shohruzbekcoder <shohruzbekcoder007@gmail.com>
2023-09-25 15:41:40 +0500

    login bootstrap

--- src/components/SignInSide/index.jsx
+++ src/components/SignInSide/index.jsx
@@ -1,38 +1,33 @@
 import React, { useState } from 'react';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-import { LoaderWrapper, LoginLogo } from './styles';
+import { AdminLogin, LoaderWrapper } from './styles';
 import { CircularProgress, Snackbar, Typography } from '@mui/material';
 import MuiAlert from '@mui/material/Alert';
-// import { useNavigate } from 'react-router-dom';
-// import { useDispatch } from 'react-redux';
-// import { token_url, user_me } from '../../utils/API_urls'
-// import { getRole, getToken } from './requests'
-// import { setUser } from '../../redux/action/userActions'
-// import { getRole as getRoleUser } from '../../utils/getRole'
-import login_pahe_img from '../../media/about-page.2f3ea62c.png' 
-import { admin_me, user_signin } from '../../utils/API_urls';
-import { getRole, getToken } from './requests';
+import login_pahe_img from '../../media/logo2.png'
+// import { admin_me, user_signin } from '../../utils/API_urls';
+// import { getRole, getToken } from './requests';
+import CustomizedInput from './CustomizedInput';
+import PersonPinCircleRoundedIcon from '@mui/icons-material/PersonPinCircleRounded';
+import LanguageSelector from './LanguageSelector';
 
 const LoadingPage = () => {
-    return (
-      <LoaderWrapper>
-        <CircularProgress color="success" />
-      </LoaderWrapper>
-    )
-  }
-  
-  const Alert = React.forwardRef(function Alert(props, ref) {
-    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
-  });
+  return (
+    <LoaderWrapper>
+      <CircularProgress color="success" />
+    </LoaderWrapper>
+  )
+}
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
 export default function SignInSide() {
 
-  // let navigate = useNavigate()
   const [pageLoading, setPageLoading] = useState(false)
   const [openAlert, setOpenAlert] = useState(false)
   const [haveatoken, setHaveatoken] = useState(false)
@@ -45,80 +40,35 @@ export default function SignInSide() {
     setOpenAlert(false);
   };
 
-  const successfulFunctionGetToken = (response) => {
-    console.log(response, response.data.token)
-    sessionStorage.setItem('x-access-token', response.data.token)
-    setHaveatoken(true)
-    getRole(admin_me, successfulFunctionGetRole, errorFunctionGetRole)
-  }
-
-  const errorFunctionGetToken = (error) => {
-    // console.log({ errorMessage: error.toString() });
-    // console.error("There was an error!", error);
-    setPageLoading(false)
-    setOpenAlert(true)
-  }
-
-  const successfulFunctionGetRole = (response) => {
-    console.log(response)
-    // dispatch(setUser(response.data))
-    // const user_role = getRoleUser(response.data)
-    // setPageLoading(false)
-    // if (user_role == "admin") {
-    //   navigate(`/${user_role}/users`)
-    // }else if (user_role == "student") {
-    //   navigate(`/${user_role}/personalplan`)
-    // }
-    // else {
-    //   navigate(`/${user_role}/dashboard`)
-    // }
-  }
-
-  const errorFunctionGetRole = (error) => {
-    // console.log({ errorMessage: error.toString() });
-    // console.error("There was an error!", error);
-    setPageLoading(false)
-    setOpenAlert(true)
-  }
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    getToken(user_signin, {
-      email: data.get('username'),
-      password: data.get('password'),
-    }, successfulFunctionGetToken, errorFunctionGetToken)
-    setPageLoading(true)
-  }
 
   return (
     <Grid container component="main" sx={{ height: '100vh' }}>
       <CssBaseline />
-      {pageLoading?<LoadingPage/>:<></>}
-    <Snackbar open={openAlert} autoHideDuration={6000} onClose={handleClose}>
-      <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
-        {haveatoken?<p>Foydalanuvchi topilmadi</p>:<p>Login yoki password noto'g'ri kiritildi</p>}
-      </Alert>
-    </Snackbar>
+      {pageLoading ? <LoadingPage /> : <></>}
+      <Snackbar open={openAlert} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
+          {haveatoken ? <p>Foydalanuvchi topilmadi</p> : <p>Login yoki password noto'g'ri kiritildi</p>}
+        </Alert>
+      </Snackbar>
       <Grid
         item
         xs={false}
         sm={4}
-        md={7}
+        md={6}
         sx={{
           backgroundImage: `url(${login_pahe_img})`,
           backgroundRepeat: 'no-repeat',
           backgroundColor: (t) =>
             t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-          backgroundSize: 'cover',
+          // backgroundSize: 'contain',
+          backgroundSize: "500px auto",
           backgroundPosition: 'center',
+          backgroundColor: "#45B5AA"
         }}
       />
-      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+      <Grid item xs={12} sm={8} md={6} component={Paper} elevation={6} square>
         <Box
           sx={{
-          //   my: 8,
-          //   mx: 4,
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center',
@@ -127,41 +77,50 @@ export default function SignInSide() {
             padding: "20px"
           }}
         >
-          <LoginLogo>
-            <img src={require('../../media/logo.png')} alt="main logo" />
-          </LoginLogo>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          id="username"
-          label="User name"
-          name="username"
-          autoComplete="username"
-          autoFocus
-        />
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          name="password"
-          label="Password"
-          type="password"
-          id="password"
-          autoComplete="current-password"
-        />
-        <Button
-          type="submit"
-          fullWidth
-          variant="contained"
-          sx={{ mt: 3, mb: 2 }}
-        >
-          Tizimga Kirish
-        </Button>
-      </Box>
+          <Box component="div" noValidate sx={{ mt: 4, p: 1 }} style={{ backgroundColor: "#E9ECF1", padding: "35px 25px", border: ' 1px solid #d6d9de', borderRadius: "12px" }}>
+            <Typography variant="h3" gutterBottom sx={{ fontWeight: "bold", fontSize: "30px" }}>
+              Kabinetga kirish
+            </Typography>
+            <Box sx={{my: 2}}>
+              <CustomizedInput label="PINFL" callback_func={() => { }} />
+            </Box>
+            <Box sx={{my: 2}}>
+              <CustomizedInput label="Parol" callback_func={() => { }} />
+            </Box>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 1, p: 2, fontSize: 15, mb: 1, borderRadius: "12px" }}
+            >
+              Kirish
+            </Button>
+            <Typography variant="subtitle1" gutterBottom sx={{ fontSize: "15px", textAlign: "center", mb: 4 }}>
+              Sizda shaxsiy kabinetingiz yo'qmi? <a href="#">Ro'yhatdan o'tish</a>
+            </Typography>
+            <Typography variant="subtitle1" gutterBottom sx={{ fontSize: "17px", maxWidth: '400px', color: "rgb(123, 123, 123)" }}>
+              Eslatma: Ushbu sahifadan Cyber Park joriy rezidentlari foydalanishlari mumkin
+            </Typography>
+          </Box>
         </Box>
       </Grid>
+      <Box sx={{
+        position: "absolute", 
+        top: 10, 
+        width: "100%", 
+        p: 1, 
+        display: 'flex', 
+        alignItems: "center", 
+        justifyContent: "flex-end", 
+        gap: '20px',
+        padding: "20px 30px 0 0"
+      }}>
+      <AdminLogin>
+        <PersonPinCircleRoundedIcon fontSize="large"/>
+        Adminlar uchun
+      </AdminLogin>
+      <LanguageSelector/>
+      </Box>
     </Grid>
-)
-        }
+  )
+}
--- src/components/SignInSide/styles.js
+++ src/components/SignInSide/styles.js
@@ -13,17 +13,75 @@ export const LoaderWrapper = styled.div`
     justify-content: center;
 `
 
-export const LoginLogo = styled.div`
-    /* width: 100%; */
-    img {
-        width: 300px;
+export const AdminLogin = styled.button`
+    box-shadow: 0 4px 12px rgba(0, 0, 0, .12);
+    background-color: #e9ecf1;
+    font-size: 14px;
+    font-family: 'Saira Semi Condensed', sans-serif;
+    font-weight: 400;
+    letter-spacing: 0.8px;
+    outline: none;
+    border: 1px solid #d6d9de;
+    color: rgba(0, 0, 0, 0.87);
+    width: 250px;
+    height: 44px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 8px;
+    cursor: pointer;
+    gap: 10px;
+`
+export const LanguageSelectorWrapper = styled.div`
+    position: relative;
+`
+
+export const SelectedLanguage = styled.div`
+    border-radius: 8px;
+    box-shadow: 0 4px 12px rgba(0, 0, 0, .12);
+    border: 1px solid #d6d9de;
+    cursor: pointer;
+    background-color: #45B5AA;
+    width: 50px;
+    height: 44px;
+    font-size: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    align-items: center;
+    color: #FFFFFF;
+`
+
+export const LanguageList = styled.ul`
+    margin: 0;
+    padding: 0;
+    border-radius: 8px;
+    overflow: hidden;
+    box-shadow: 0 4px 12px rgba(0, 0, 0, .12);
+    width: 50px;
+    position: absolute;
+    top: 50px;
+    /* visibility: hidden; */
+    /* transition: all .5s; */
+    display: ${props => props.open? 'block':'none'};
+`
+
+export const LanguageListItem = styled.li`
+    margin: 0;
+    padding: 0;
+    cursor: pointer;
+    width: 100%;
+    height: 44px;
+    font-size: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    align-items: center;
+    color: #45B5AA;
+    background-color: #FFFFFF;
+    transition: all .5s;
+    :hover {
+        background-color: #45B5AA;
+        color: #FFFFFF;
     }
-    @media screen and (max-width: 400px) {
-        width: 200px;
-        img {
-            display: inline-block;
-            width: 100%;
-        }
-    }
-    
 `
